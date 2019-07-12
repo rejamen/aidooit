8f68e4837d9d9ddc0c5b911cd16ded97fde7390b
Reinaldo J. Menendez Alonso <rejamen@gmail.com>
2019-07-12 09:35:51 -0400

    Added blank=True to Person and deafult time to history

--- aidooit_people/admin.py
+++ aidooit_people/admin.py
@@ -1,6 +1,24 @@
 """Register modules to admin."""
 from django.contrib import admin
-from .models import Person
+from .models import Person, PersonHistory
 # Register your models here.
 
-admin.site.register(Person)
+
+class HistoryInline(admin.TabularInline):
+	"""Set inline records."""
+
+	model = PersonHistory
+	extra = 1
+
+
+class PersonAdmin(admin.ModelAdmin):
+	"""Modify what to admin in Person model."""
+
+#	fields = ['name', 'last_name'] #use this only for change the order
+	fieldsets = [
+		(None, {'fields': ['name', 'last_name']}),
+		('Other data', {'fields': ['mobile', 'phone', 'email']})
+	]
+	inlines = [HistoryInline]
+
+admin.site.register(Person, PersonAdmin)
--- aidooit_people/models.py
+++ aidooit_people/models.py
@@ -1,5 +1,6 @@
 """Model for aidooit people app."""
 from django.db import models
+from django.utils import timezone
 
 
 class Person(models.Model):
@@ -7,11 +8,22 @@ class Person(models.Model):
 
     name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=100)
-    mobile = models.CharField(max_length=20)
-    phone = models.CharField(max_length=20)
-    email = models.CharField(max_length=40)
+    mobile = models.CharField(max_length=20, blank=True)
+    phone = models.CharField(max_length=20, blank=True)
+    email = models.CharField(max_length=40, blank=True)
 
     def __str__(self):
         """How str is displayed."""
         res = '{} {}'.format(self.name, self.last_name)
         return res
+
+
+class PersonHistory(models.Model):
+    """Set of historical records."""
+
+    date = models.DateTimeField(default=timezone.now())
+    description = models.CharField(max_length=500)
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = "Person Historical"
