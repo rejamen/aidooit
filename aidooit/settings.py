68b8215f2e30e248728a75cf4f9320166ce0147c
Reinaldo J. Menendez Alonso <rejamen@gmail.com>
2019-09-09 03:14:33 -0400

    Set pagination 10 to Rest API

--- aidooit/settings.py
+++ aidooit/settings.py
@@ -44,6 +44,11 @@ INSTALLED_APPS = [
     'aidooit_access_point',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
